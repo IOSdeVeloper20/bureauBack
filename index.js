d97575a142fe54d7bd9974882c113c32e4d2da38
IOSdeVeloper20 <81517945+IOSdeVeloper20@users.noreply.github.com>
2024-10-12 20:05:42 +0300

    MONGODB_URI error fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const loginRouter = require("./Routes/LoginRouter");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
